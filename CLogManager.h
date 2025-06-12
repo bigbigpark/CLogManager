172b3908a656c6deaf1e04d04a3f4df9b7d806a5
Seongchang Park <72936863+bigbigpark@users.noreply.github.com>
2025-06-12 15:28:09 +0900

    First commit

---
+++ CLogManager.h
@@ -0,0 +1,87 @@
+/*****************************************************************//**
+ * \file   CLogManager.cpp
+ * \brief  로그 관리 클래스 구현
+ *
+ * \author Seongchang Park
+ * \date   26 November 2024
+ * 
+ * \copyright
+ * MIT License
+ * 
+ * Copyright (c) 2024 Seongchang Park
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *********************************************************************/
+#pragma once
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <afxstr.h>
+#include <fstream>
+#include <filesystem>
+#include <chrono>
+#include <afx.h>
+#include <cstdarg>  // va_list
+
+// 로그 매크로 (외부에서는 이 함수들만 사용해야 함)
+#define LOG_TRACE(msg, ...)CLogManager::getInstance().logMessage(LogLevel::LOG_TRACE, msg, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define LOG_DEBUG(msg, ...)CLogManager::getInstance().logMessage(LogLevel::LOG_DEBUG, msg, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define LOG_INFO(msg, ...) CLogManager::getInstance().logMessage(LogLevel::LOG_INFO, msg, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define LOG_WARN(msg, ...) CLogManager::getInstance().logMessage(LogLevel::LOG_WARN, msg, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define LOG_ERROR(msg, ...)CLogManager::getInstance().logMessage(LogLevel::LOG_ERROR, msg, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define LOG_FATAL(msg, ...)CLogManager::getInstance().logMessage(LogLevel::LOG_FATAL, msg, __FUNCTION__, __LINE__, __VA_ARGS__)
+
+enum class LogLevel
+{
+	LOG_TRACE,
+	LOG_DEBUG,
+	LOG_INFO,
+	LOG_WARN,
+	LOG_ERROR,
+	LOG_FATAL
+};
+
+class CLogManager
+{
+	CLogManager();
+	~CLogManager();
+
+public:
+	static CLogManager& getInstance(); // 클래스 인스턴스 반환 (singleton)
+	void logMessage(LogLevel level, const char* msg, const char* function, int line, ...); // 로그 메세지 작성 함수
+
+private:
+	std::queue<std::pair<LogLevel, std::string>> log_queue_; // 로그 queue
+	std::mutex mutex_;				// 동기화용 mutex
+	std::condition_variable cv_;	// 대기/알림을 위한 conditional variable
+	std::thread log_worker_thread_;	// 로그 처리하는 work thread
+
+	std::fstream current_log_file_; // 현재 로그 파일
+	size_t current_log_file_size_;	// 현재 로그 파일 사이즈
+	std::string log_directory_;		// 로그 디렉토리
+	LogLevel log_level_;			// 로그 레벨
+
+	void readInitialLogSetting();	// 로그 레벨 읽어오기
+	void logWorker(); // 로그 처리 worker 스레드 함수
+	std::string convertLogLevelToString(LogLevel level); // 로그 레벨을 문자열로 변환
+	void writeLogToFile(const std::pair<LogLevel, std::string>& message); // 로그를 텍스트 파일로 저장
+};
+// EOF
--- README.md
+++ README.md
@@ -1,2 +1,98 @@
 # CLogManager
+
 A lightweight and easy-to-use C++ logging class that supports multi-threaded environments.
+
+</br>
+
+## ✨ Features
+- Thread-safe class (Singleton access)
+- Easy to use with macro function
+- Various Log levels (TRACE, DEBUG, INFO, WARN, ERROR, FATAL)
+- Automatic log file rotation (100 MB max per file)
+- Logs stored in per-day folders (e.g., `C:\MyLog\20250612`)
+- Configurable log level via `log_conf.ini`
+
+
+</br>
+
+## 📁 File Structure
+- `CLogManager.h` – Header file for the logging manager
+- `CLogManager.cpp` – Implementation file
+
+</br>
+
+## 🔧 Configuration
+
+Write `C:/MyLog/log_conf.ini` with the following content:
+
+~~~ini
+[LOG_SETTING]
+log_level = 1
+# 0: trace, 1: debug, 2: info, 3: warn, 4: error, 5: fatal
+~~~
+
+<br/>
+
+## 🛠️ Usage
+
+### Include the header and declare the class object
+~~~cpp
+#include "CLogManager.h"
+
+
+int main ()
+{
+    CLogManager CLogManager;
+
+    return 0;
+}
+~~~
+
+<!-- <br/> -->
+
+### Log a message macro function
+~~~cpp
+void callbackFuncB()
+{
+    LOG_DEBUG("this is callback B");
+}
+
+void callbackFuncA()
+{
+    LOG_INFO("this is callback A");
+
+    // Possible Execption..
+    LOG_ERROR("Error occured :(");
+}
+~~~
+
+<br/>
+
+## 📂 Example Log Output
+Logged path : `C:\MyLog\20250612\20250612_134508.txt`
+
+~~~rust
+[2025-06-12 13:45:30.112] [INFO] callbackFuncA (Line : 5) -> this is callback A
+[2025-06-12 13:45:30.114] [DEBUG] callbackFuncB (Line : 2) -> this is callback B
+[2025-06-12 13:45:30.113] [ERROR] callbackFuncA (Line : 8) -> Error occured :(
+~~~
+
+<br/>
+
+## 📌 Notes
+* Log files are automatically created inside daily folders `C:\MyLog\YYYYMMDD`.
+* If a log file exceeds **100 MB**, a new file with timestamp is created.
+* Log output is buffered via an internal queue and flushed by a worker thread.
+* Uses standard C++17 (`<thread>`, `<mutex>`, `<chrono>`, `<filesystem>`).
+
+<br/>
+
+## 📄 License
+
+This project is licensed under the MIT License – see the [LICENSE](./LICENSE) file for details.
+
+<br/>
+
+## 🧑‍💻 Author
+- Name: Seongchang Park
+- Email: scsc1125@gmail.com
